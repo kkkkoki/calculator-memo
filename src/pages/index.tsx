fe5db2266b8c4e7312e9adf92920790f2b6943d8
kkkkoki <koki_inoue45@icloud.com>
2023-04-16 15:55:53 +0900

    add meta favicon layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,30 @@
-import ThemeToggleButton from "@/components/ThemeToggleButton";
+import { ReactElement } from 'react';
+import Head from 'next/head';
+import { NextPageWithLayout } from './_app';
+import Layout from '@/components/base/Layout';
 
-const Home = () => {
+const Home: NextPageWithLayout = () => {
   return (
-    <div>
-      <div>Hello World</div>
-      <ThemeToggleButton />
-    </div>
+    <>
+      <Head>
+        <title>Calculator Memo</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="計算機&メモ。計算をより便利に！" />
+        <link
+          rel="icon"
+          href="https://cdn.jsdelivr.net/gh/jdecked/twemoji@latest/assets/svg/1f9ee.svg"
+        />
+      </Head>
+
+      <div>
+        <div>Hello World</div>
+      </div>
+    </>
   );
+};
+
+Home.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
 };
 
 export default Home;
