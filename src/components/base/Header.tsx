32e4bec133354d6a3b1441d650946b83eaff1eaf
kkkkoki <koki_inoue45@icloud.com>
2023-04-16 17:28:09 +0900

    add header

--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
+import Link from 'next/link';
+import ThemeToggleButton from '../ThemeToggleButton';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="flex items-center justify-between h-16">
+      <Link className="" href="/">
+        <h1 className="text-xl font-bold">CLMEMO</h1>
+      </Link>
+      <ThemeToggleButton />
+    </header>
+  );
 };
 
 export default Header;
