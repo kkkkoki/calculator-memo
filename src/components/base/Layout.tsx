743e65ba0a5e3aa310107153bc76768512c804cb
kkkkoki <koki_inoue45@icloud.com>
2023-04-16 17:27:35 +0900

    add container

--- src/components/base/Layout.tsx
+++ src/components/base/Layout.tsx
@@ -4,11 +4,11 @@ import Header from './Header';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <>
+    <div className="container flex flex-col min-h-screen">
       <Header />
-      <main>{children}</main>
+      <main className="flex-1">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        "bgc-light": "#f0e7db",
-        "bgc-dark": "#202320",
-        "text-color-light": "#1a202c",
-        "text-color-dark": "#fff",
+        'bgc-light': '#f0e7db',
+        'bgc-dark': '#202320',
+        'text-color-light': '#1a202c',
+        'text-color-dark': '#fff',
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
       },
     },
   },
   plugins: [],
-  darkMode: "class",
+  darkMode: 'class',
 };
