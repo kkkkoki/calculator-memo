bfcdb5625013e785885dd7555e3500f12b461a10
kkkkoki <koki_inoue45@icloud.com>
2023-04-16 15:48:32 +0900

    format

--- src/components/base/Layout.tsx
+++ src/components/base/Layout.tsx
@@ -1,6 +1,6 @@
-import React, { ReactNode } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
+import React, { ReactNode } from 'react';
+import Footer from './Footer';
+import Header from './Header';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
