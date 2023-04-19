70c3e153f214cd4a55f8461d4f7b81dba6833bff
kkkkoki <koki_inoue45@icloud.com>
2023-04-19 23:05:24 +0900

    計算ロジック実装

---
+++ src/components/calculator/calculate.ts
@@ -0,0 +1,138 @@
+export interface State {
+  current: string;
+  operand: number;
+  operator: string | null;
+  isNextClear: boolean;
+}
+
+export const isNumber = (val: string) => {
+  const numRegex = /[0-9]/;
+  return numRegex.test(val);
+};
+
+export const calculate = (value: string, state: State): State => {
+  if (isNumber(value)) {
+    return handleNumber(value, state);
+  } else {
+    switch (value) {
+      case '=':
+        return hadleEqual(state);
+      case '.':
+        return handleDot(state);
+      case 'AC':
+        return handleAllClear();
+      default:
+        return handleOperator(value, state);
+    }
+  }
+};
+
+const handleNumber = (value: string, state: State): State => {
+  return {
+    // currentが0 or state.isNextClear(operator押下)の時はvalueを返す
+    current:
+      state.current === '0' || state.isNextClear
+        ? value
+        : state.current + value,
+    operand: state.operand,
+    operator: state.operator,
+    isNextClear: false,
+  };
+};
+
+const handleOperator = (value: string, state: State): State => {
+  if (state.operator === null) {
+    return {
+      current: state.current,
+      operand: parseFloat(state.current),
+      operator: value,
+      isNextClear: true,
+    };
+  }
+
+  const nextValue = operate(state);
+  return {
+    current: `${nextValue}`,
+    operand: nextValue,
+    operator: value,
+    isNextClear: true,
+  };
+};
+
+const handleDot = (state: State): State => {
+  if (state.current.indexOf('.') !== -1) {
+    return state;
+  }
+
+  return {
+    current: state.current + '.',
+    operand: state.operand,
+    operator: state.operator,
+    isNextClear: false,
+  };
+};
+
+// "C"で今のstateをクリアする
+// const handleClear = (state: State): State => {
+//   return {
+//     current: '0',
+//     operand: state.operand,
+//     operator: state.operator, // ここはnullかも?
+//     isNextClear: false,
+//   };
+// };
+
+const handleAllClear = (): State => {
+  return {
+    current: '0',
+    operand: 0,
+    operator: null,
+    isNextClear: false,
+  };
+};
+
+// "D"で1文字削除(既に1文字なら0にする)
+// const handleDelete = (state: State): State => {
+//   return {
+//     current:
+//       state.current.length === 1
+//         ? '0'
+//         : state.current.substring(0, state.current.length - 1),
+//     operand: state.operand,
+//     operator: state.operator,
+//     isNextClear: false,
+//   };
+// };
+
+const hadleEqual = (state: State): State => {
+  if (state.operator === null) {
+    return state;
+  }
+
+  const nextValue = operate(state);
+  return {
+    current: `${nextValue}`,
+    operand: 0,
+    operator: null,
+    isNextClear: true,
+  };
+};
+
+const operate = (state: State): number => {
+  const current = parseFloat(state.current);
+
+  switch (state.operator) {
+    case '+':
+      return state.operand + current;
+    case '-':
+      return state.operand - current;
+    case '×':
+      return state.operand * current;
+    case '/':
+      return state.operand / current;
+    default:
+      break;
+  }
+
+  return current;
+};
