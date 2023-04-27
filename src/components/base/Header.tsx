c13be82675aead260491a4661adef4a4998c7455
kkkkoki <koki_inoue45@icloud.com>
2023-04-28 01:39:09 +0900

    add ヘッダーにlayoutの切り替えボタン作成。切り替えの値をstorageとstateで管理

--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
+import { ArrowsRightLeftIcon } from '@heroicons/react/24/solid';
 import ThemeToggleButton from '../ThemeToggleButton';
 
-const Header = () => {
+type HeaderProps = {
+  direction: string | null;
+  setDirection: VoidFunction;
+};
+
+const Header: React.FC<HeaderProps> = ({ direction, setDirection }) => {
   return (
     <header className="flex items-center justify-between h-16">
-      <Link className="" href="/">
+      <Link href="/">
         <h1 className="text-xl font-bold">CLMEMO</h1>
       </Link>
       <ThemeToggleButton />
+      <button onClick={setDirection}>
+        <ArrowsRightLeftIcon className={`w-6 h-6`} />
+      </button>
     </header>
   );
 };
--- src/components/base/Layout.tsx
+++ src/components/base/Layout.tsx
@@ -1,14 +1,47 @@
-import React, { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import Footer from './Footer';
 import Header from './Header';
+import { LayoutDirectionType } from '@/types/layoutDirection';
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  return (
+  const [direction, setDirection] = useState<LayoutDirectionType | null>(null);
+  const [isRendered, setIsRendered] = useState(false);
+
+  useEffect(() => {
+    setIsRendered(true);
+  }, []);
+
+  useEffect(() => {
+    if (direction === null) {
+      setDirection(JSON.parse(localStorage.getItem('directionState') + ''));
+      return;
+    }
+    localStorage.setItem('directionState', JSON.stringify(direction));
+  }, [direction]);
+
+  return isRendered ? (
     <div className="container flex flex-col min-h-screen">
-      <Header />
-      <main className="flex items-center my-16 flex-1">{children}</main>
+      <Header
+        direction={direction}
+        setDirection={() => setDirection(direction === 'LTR' ? 'RTL' : 'LTR')}
+      />
+      <div
+        className={`flex flex-1 items-center ${
+          direction === 'LTR' ? 'flex-row' : 'flex-row-reverse'
+        }`}
+      >
+        <div className="w-40 text-center">
+          <p>hello</p>
+        </div>
+        <main className="flex items-center my-16 flex-1">{children}</main>
+        <div className="w-40 text-center">
+          <p>world</p>
+        </div>
+      </div>
       <Footer />
     </div>
+  ) : (
+    <></>
   );
 };
 
