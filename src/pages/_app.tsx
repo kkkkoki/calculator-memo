3d95beb53a6fe702e956090b5f533e00cf508a72
kkkkoki <koki_inoue45@icloud.com>
2023-05-03 02:41:40 +0900

    ✨ faviconコンポーネント作成&NODE_ENVがdevelopmentの時favicon追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { ThemeProvider } from "next-themes";
-import { ReactElement, ReactNode } from "react";
-import { NextPage } from "next";
+import { ReactElement, ReactNode } from 'react';
+import { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from 'next-themes';
+import Favicon from '@/components/base/Favicon';
+import '@/styles/globals.css';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -16,8 +18,16 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <ThemeProvider attribute="class">
-      {getLayout(<Component {...pageProps} />)}
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Calculator Memo</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="計算機&メモ。計算をより便利に！" />
+        <Favicon />
+      </Head>
+      <ThemeProvider attribute="class">
+        {getLayout(<Component {...pageProps} />)}
+      </ThemeProvider>
+    </>
   );
 }
