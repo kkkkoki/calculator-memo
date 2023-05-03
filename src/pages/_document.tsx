3531357be6809e82d9fcb74839bd36ef1916068c
kkkkoki <koki_inoue45@icloud.com>
2023-05-04 07:56:36 +0900

    seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Favicon from '@/_seo/Favicon';
 
 export default function Document() {
   return (
-    <Html lang="ja">
-      <Head />
+    <Html lang="ja" prefix="og:http://ogp.me/ns#">
+      <Head>
+        <Favicon />
+      </Head>
       <body>
         <Main />
         <NextScript />
