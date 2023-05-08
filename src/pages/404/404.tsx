a354c825a0cb3a4381385d96e85467c735ecccca
kkkkoki <koki_inoue45@icloud.com>
2023-05-09 00:59:02 +0900

    ✨  NotFoundページのコーディング

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,5 +1,38 @@
 import { FC } from 'react';
+import Link from 'next/link';
+import { tv } from 'tailwind-variants';
+
+const notFound = tv({
+  slots: {
+    root: 'flex items-center justify-center h-screen py-6 sm:py-8 lg:py-12',
+    flex: 'flex flex-col items-center',
+    logo: 'text-black-800 mr-6 mb-8 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl',
+    accent:
+      'mb-4 text-sm font-semibold uppercase text-orange-400 dark:text-orange-600 md:text-base',
+    title: 'mb-2 text-center text-2xl font-bold md:text-3xl',
+    subTitle: 'mb-12 max-w-screen-md text-center opacity-70 md:text-lg',
+    home: 'inline-block rounded-lg px-8 py-3 text-center text-sm font-semibold outline-none focus-visible:ring md:text-base',
+  },
+});
 
 export const NotFound: FC = () => {
-  return <h1>404 - Page Not Found</h1>;
+  const { root, flex, logo, accent, title, subTitle, home } = notFound();
+  return (
+    <div className={root()}>
+      <div className={flex()}>
+        <Link href="/" className={logo()} aria-label="logo">
+          🧮CLMEMO
+        </Link>
+
+        <p className={accent()}>That’s a 404</p>
+        <h1 className={title()}>Page not found</h1>
+
+        <p className={subTitle()}>存在しないページです</p>
+
+        <Link href="/" className={home()}>
+          ホーム
+        </Link>
+      </div>
+    </div>
+  );
 };
--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,12 +1,5 @@
-import { FC } from 'react';
 import { NotFound } from './404';
 
-const CustomErrorPage: FC = () => {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <NotFound />
-    </div>
-  );
-};
-
-export default CustomErrorPage;
+export default function () {
+  return <NotFound />;
+}
