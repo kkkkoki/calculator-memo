7ec39966b4de487f1dee8c4d74a10d42c8bb3ee4
kkkkoki <koki_inoue45@icloud.com>
2023-05-27 22:38:30 +0900

    404でlayoutが表示されないように修正

---
+++ src/app/(calculator)/layout.tsx
@@ -0,0 +1,6 @@
+import "@/styles/globals.css";
+import Layout from "@/components/base/Layout";
+
+export default function CalculatorLayout({ children }: { children: React.ReactNode }) {
+  return <Layout>{children}</Layout>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Metadata } from "next";
 import "@/styles/globals.css";
 import config from "@/config/nextSeoConfig";
 
-import Layout from "@/components/base/Layout";
-
 import Providers from "../components/providers";
 
 export const metadata: Metadata = {
@@ -15,9 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ja" suppressHydrationWarning={true}>
       <body className="bg-brown-4 dark:bg-sand-1 text-brown-12">
-        <Providers>
-          <Layout>{children}</Layout>
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
