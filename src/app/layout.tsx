48a66aa0693f5b61b7c66f1a3706f93b2a24f2b7
kkkkoki <koki_inoue45@icloud.com>
2023-05-27 21:28:28 +0900

    🔧 app dir対応のためにlayout.tsxにLayout.tsxを配置

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Metadata } from "next";
 import "@/styles/globals.css";
 import config from "@/config/nextSeoConfig";
 
+import Layout from "@/components/base/Layout";
+
 import Providers from "../components/providers";
 
 export const metadata: Metadata = {
@@ -12,8 +14,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja" suppressHydrationWarning={true}>
-      <body>
-        <Providers>{children}</Providers>
+      <body className="bg-brown-4 dark:bg-sand-1 text-brown-12">
+        <Providers>
+          <Layout>{children}</Layout>
+        </Providers>
       </body>
     </html>
   );
